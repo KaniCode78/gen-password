049f84b38d0cbb395d017a2204f3243a0d3e8a6c
Kaniltazo <angeldanielrm@icloud.com>
2024-09-17 16:57:43 -0500

    update boton limpiar

--- main.js
+++ main.js
@@ -2,7 +2,17 @@ let cantidad = document.getElementById('cantidad');
 let boton = document.getElementById('generar');
 let contrasena = document.getElementById('contrasena');
 
-const cadenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012345678';
+const cadenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012345678!@#$%^&*'; //solo agregue los caracteres especiales
+const limpiarBtn = document.getElementById('limpiarBtn');
+
+// funcion limpiar el input de la contrana
+limpiarBtn.addEventListener('click', function () {
+    contrasena.value = ''; 
+});
+
+
+
+//funcion para genarar el password
 
 function generar() {
 
@@ -18,13 +28,15 @@ function generar() {
 
         let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
 
-        password+=caracterAleatorio;
+        password += caracterAleatorio;
     }
 
     contrasena.value = password;
 
 }
 
+
+//pasword
 
 
 
